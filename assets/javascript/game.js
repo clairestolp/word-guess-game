ffa5177c9c9c7b8d5a9083b4dd66fbe78d906d8c
Claire Stolp <clairemstolp@gmail.com>
2018-05-01 18:40:18 -0400

    created js script and added basic html styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,583 @@
+var pokemon = [
+  {
+    "name": "Bulbasaur",
+    "number": 1,
+    "type": "grass/poison",
+    "img": "https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/240px-001Bulbasaur.png",
+  },
+  {
+    "name": "Ivysaur",
+    "number": 2,
+    "type": "grass/poison",
+    "img": "",
+  },
+  {
+    "name": "Venusaur",
+    "number": 3,
+    "type": "grass/poison",
+    "img": "",
+  },
+  {
+    "name": "Charmander",
+    "number": 4,
+    "type": "Fire",
+    "img": "",
+  },
+  {
+    "name": "Charmeleon",
+    "number": 5,
+    "type": "Fire",
+    "img": "",
+  },
+  {
+    "name": "Charizard",
+    "number": 6,
+    "type": "Fire/Flying",
+    "img": "",
+  },
+  {
+    "name": "Squirtle",
+    "number":  7,
+    "type": "Water",
+    "img": "",
+  },
+  {
+    "name": "Wartortle",
+    "number": 8,
+    "type": "Water",
+    "img": "",
+  },
+  {
+    "name": "Caterpie",
+    "number": 10,
+    "type": "Bug",
+    "img": "",
+  },
+  {
+    "name": "Metapod",
+    "number": 11,
+    "type": "Bug",
+    "img": "",
+  },
+  {
+    "name": "Butterfree",
+    "number": 12,
+    "type": "Bug/Flying",
+    "img": "",
+  },
+  {
+    "name": "Weedle",
+    "number": 13,
+    "type": "Bug/Poison",
+    "img": "",
+  },
+  {
+    "name": "Kakuna",
+    "number": 14,
+    "type": "Bug/Poison",
+    "img": "",
+  },
+  {
+    "name": "Beedrill",
+    "number": 15,
+    "type": "Bug/Poison",
+    "img": "",
+  },
+  {
+    "name": "Pidgey",
+    "number": 16,
+    "type": "Normal/Flying",
+    "img": "",
+  },
+  {
+    "name": "Pidgeotto",
+    "number": 17,
+    "type": "Normal/Flying",
+    "img": "",
+  },
+  {
+    "name": "Pidgeotto",
+    "number": 18,
+    "type": "Normal/Flying",
+    "img": "",
+  },
+  {
+    "name": "Rattata",
+    "number": 19,
+    "type": "Normal",
+    "img": "",
+  },
+  {
+    "name": "Raticate",
+    "number": 20,
+    "type": "Normal",
+    "img": "",
+  },
+  {
+    "name": "Spearow",
+    "number": 21,
+    "type": "Normal/Flying",
+    "img": "",
+  },
+  {
+    "name": "Fearow",
+    "number": 22,
+    "type": "Normal/Flying",
+    "img": "",
+  },
+  {
+    "name": "Ekans",
+    "number": 23,
+    "type": "Poison",
+    "img": "",
+  },
+  {
+    "name": "Arbok",
+    "number": 24,
+    "type": "Poison",
+    "img": "",
+  },
+  {
+    "name": "Pikachu",
+    "number": 25,
+    "type": "Electric",
+    "img": "",
+  },
+  {
+    "name": "Raichu",
+    "number": 26,
+    "type": "Electric",
+    "img": "",
+  },
+  {
+    "name": "Sandshrew",
+    "number": 27,
+    "type": "Ground",
+    "img": "",
+  },
+  {
+    "name": "Sandslash",
+    "number": 28,
+    "type": "Ground",
+    "img": "",
+  },
+  {
+    "name": "Nidoran",
+    "number": 29,
+    "type": "Poison",
+    "img": "",
+  },
+  {
+    "name": "Nidorina",
+    "number": 30,
+    "type": "Poison",
+    "img": "",
+  },
+  {
+    "name": "Nidoqueen",
+    "number": 31,
+    "type": "Posion/Ground",
+    "img": "",
+  },
+  {
+    "name": "Nidoran",
+    "number": 32,
+    "type": "Poison",
+    "img": "",
+  },
+  {
+    "name": "Nidorino",
+    "number": 33,
+    "type": "Poison",
+    "img": "",
+  },
+  {
+    "name": "Nidoking",
+    "number": 34,
+    "type": "Poison/Ground",
+    "img": "",
+  },
+  {
+    "name": "Clefairy",
+    "number": 35,
+    "type": "Fairy",
+    "img": "",
+  },
+  {
+    "name": "Clefable",
+    "number": 36,
+    "type": "Fairy",
+    "img": "",
+  },
+  {
+    "name": "Vulpix",
+    "number": 37,
+    "type": "Fire",
+    "img": "",
+  },
+  {
+  "name": "Ninetails",
+  "number": 38,
+  "type": "Fire",
+  "img": "",
+  },
+  {
+    "name": "Jigglypuff",
+    "number": 39,
+    "type": "Normal/Fairy",
+    "img": "",
+  },
+  {
+    "name": "Wigglytuff",
+    "number": 40,
+    "type": "Normal/Fairy",
+    "img": "",
+  },
+  {
+    "name": "Zubat",
+    "number": 41,
+    "type": "Poison/Flying",
+    "img": "",
+  },
+  {
+    "name": "Golbat",
+    "number": 42,
+    "type": "Poison/Flying",
+    "img": "",
+  },
+  {
+    "name": "Oddish",
+    "number": 43,
+    "type": "Grass/Poison",
+    "img": "",
+  },
+  {
+    "name": "Gloom",
+    "number": 44,
+    "type": "Grass/Poison",
+    "img": "",
+  },
+  {
+    "name": "Vileplume",
+    "number": 45,
+    "type": "Grass/Poison",
+    "img": "",
+  },
+  {
+    "name": "Paras",
+    "number": 46,
+    "type": "Bug/Grass",
+    "img": "",
+  },
+  {
+    "name": "Parasect",
+    "number": 47,
+    "type": "Bug/Grass",
+    "img": "",
+  },
+  {
+    "name": "Venonat",
+    "number": 48,
+    "type": "Bug/Poison",
+    "img": "",
+  },
+  {
+    "name": "Venomoth",
+    "number": 49,
+    "type": "Bug/Poison",
+    "img": "",
+  },
+  {
+    "name": "Diglett",
+    "number": 50,
+    "type": "Ground",
+    "img": "",
+  },
+  {
+    "name": "Dugtrio",
+    "number": 51,
+    "type": "Ground",
+    "img": "",
+  },
+  {
+    "name": "Meowth",
+    "number": 52,
+    "type": "Normal",
+    "img": "",
+  },
+  {
+    "name": "Persian",
+    "number": 53,
+    "type": "Normal",
+    "img": "",
+  },
+  {
+    "name": "Psyduck",
+    "number": 54,
+    "type": "Water",
+    "img": "",
+  },
+  {
+    "name": "Golduck",
+    "number": 55,
+    "type": "Water",
+    "img": "",
+  },
+  {
+    "name": "Mankey",
+    "number": 56,
+    "type": "Fighting",
+    "img": "",
+  },
+  {
+    "name": "Primape",
+    "number": 57,
+    "type": "Fighting",
+    "img": "",
+  },
+  {
+    "name": "Growlithe",
+    "number": 58,
+    "type": "Fire",
+    "img": "",
+  },
+  {
+    "name": "Arcanine",
+    "number": 59,
+    "type": "Fire",
+    "img": "",
+  },
+  {
+    "name": "Poliwag",
+    "number": 60,
+    "type": "Water",
+    "img": "",
+  },
+  {
+    "name": "Poliwhirl",
+    "number": 61,
+    "type": "Water",
+    "img": "",
+  },
+  {
+    "name": "Poliwrath",
+    "number": 62,
+    "type": "Water/Fighting",
+    "img": "",
+  },
+  {
+    "name": "Abra",
+    "number": 64,
+    "type": "Psychic",
+    "img": "",
+  },
+
+  {
+    "name": "Kadabra",
+    "number": 64,
+    "type": "Psychic",
+    "img": "",
+  },
+  {
+    "name": "Alakazam",
+    "number": 65,
+    "type": "Psychic",
+    "img": "",
+  },
+  {
+    "name": "Machop",
+    "number": 66,
+    "type": "Fighting",
+    "img": "",
+  },
+  {
+    "name": "Machoke",
+    "number": 67,
+    "type": "Fighting",
+    "img": "",
+  },
+  {
+    "name": "Machamp",
+    "number": 68,
+    "type": "Fighing",
+    "img": "",
+  },
+  {
+    "name": "Bellsprout",
+    "number": 69,
+    "type": "Grass/Poison",
+    "img": "",
+  },
+  {
+    "name": "Weepinbell",
+    "number": 70,
+    "type": "Grass/Poison",
+    "img": "",
+  },
+  {
+    "name": "Victreebel",
+    "number": 71,
+    "type": "Grass/Poison",
+    "img": "",
+  },
+  {
+    "name": "Tentacool",
+    "number": 72,
+    "type": "Water/Poison",
+    "img": "",
+  },
+  {
+    "name": "Tentacruel",
+    "number": 73,
+    "type": "Water/Poison",
+    "img": "",
+  },
+  {
+    "name": "Geodude",
+    "number": 74,
+    "type": "Rock/Ground",
+    "img": "",
+  },
+  {
+    "name": "Gravler",
+    "number": 75,
+    "type": "Rock/Ground",
+    "img": "",
+  },
+  {
+    "name": "Golem",
+    "number": 76,
+    "type": "Rock/Ground",
+    "img": "",
+  },
+  {
+    "name": "Ponyta",
+    "number": 78,
+    "type": "Fire",
+    "img": "",
+  },
+  {
+    "name": "Rapidash",
+    "number": 78,
+    "type": "Fire",
+    "img": "",
+  },
+  {
+    "name": "Slowpoke",
+    "number": 79,
+    "type": "Water/Psychic",
+    "img": "",
+  },
+  {
+    "name": "Slowbro",
+    "number": 80,
+    "type": "Water/Psychic",
+    "img": "",
+  },
+  {
+    "name": "Magnemite",
+    "number": 81,
+    "type": "Electric/Steel",
+    "img": "",
+  },
+  {
+    "name": "Magneton",
+    "number": 82,
+    "type": "Electric/Steel",
+    "img": "",
+  },
+  {
+    "name": "Farfetchd",
+    "number": 83,
+    "type": "Normal/Flying",
+    "img": "",
+  },
+  {
+    "name": "Doduo",
+    "number": 84,
+    "type": "Normal/Flying",
+    "img": "",
+  }
+];
+
+var rng = Math.floor(Math.random() * 22);
+var guesses = 6;
+var lettersUsed = '';
+var problemArr = [];
+var solutionArr = [];
+var win = false;
+
+
+function generateProblem (length, input, index) {
+  var problemString = '';
+  if(!input){
+    input = '_';
+    for (var i = 0; i < length; i++){
+      problemArr.push(input);
+      solutionArr.push(pokemon[rng].name.charAt(i).toUpperCase());
+    }
+  }else{
+    problemArr[index] = input;
+    solutionArr[index] = '_';
+    console.log(solutionArr);
+    }
+
+    for(var j = 0; j < length; j++){
+      problemString = problemString + ' ' + problemArr[j];
+    }
+
+    document.getElementById("game").innerHTML =
+    '<br><br>' + problemString + '<br><br>';
+  }
+
+
+console.log(pokemon[rng]);
+generateProblem(pokemon[rng].name.length);
+console.log(solutionArr);
+
+document.onkeyup = function () {
+  var userInput = event.key.toUpperCase();
+  console.log("user input: ", userInput);
+  var pokename = pokemon[rng].name.toUpperCase();
+
+  if(problemArr.includes(userInput)){
+    return alert('You already guessed that letter!');
+  }
+
+  if (userInput.charCodeAt(0) <= 90 && userInput.charCodeAt(0) >= 65 && userInput.length === 1){
+    if(!solutionArr.includes(userInput)){
+      guesses--;
+      lettersUsed = lettersUsed + ' ' + userInput;
+    }else{
+      while(solutionArr.includes(userInput)){
+        generateProblem(problemArr.length, userInput.toUpperCase(), solutionArr.indexOf(userInput));
+      }
+      lettersUsed = lettersUsed + ' ' + userInput;
+    }
+    console.log("guesses remaining: ", guesses);
+    console.log(lettersUsed);
+    document.getElementById('remaining').innerHTML = guesses;
+    document.getElementById('letters').innerHTML = lettersUsed;
+  }
+
+  if(guesses === 0){
+    alert('Voltorb used Self Destruct');
+    alert('You fainted!');
+    location.reload();
+  }
+
+/*
+  for(var k = 0; k < problemArr.length; k++){
+    if(problemArr[k] !== "_"){
+      win = true;
+    }else{
+      win = false;
+    }
+  }
+
+  if(win){
+    alert('Voltorb looks happy!');
+    alert('Voltorb ran away!');
+    location.reload();
+  }*/
+}
